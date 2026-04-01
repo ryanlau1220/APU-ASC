1404e825c558ccb065ec1b95ecddfe3d10415c9d
ryanlau1220 <liujunhong20@gmail.com>
2026-04-01 14:53:13 +0800

    refactor(ui): migrate to java native nimbus look and feel

--- src/main/java/com/apu/asc/Main.java
+++ src/main/java/com/apu/asc/Main.java
@@ -5,7 +5,11 @@ import javax.swing.SwingUtilities;
 
 public class Main {
   public static void main(String[] args) {
-    com.formdev.flatlaf.FlatLightLaf.setup();
+    try {
+      javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    } catch (Exception ex) {
+      System.err.println("Failed to initialize Nimbus Look and Feel");
+    }
     SwingUtilities.invokeLater(() -> new AppShell().setVisible(true));
   }
 }
