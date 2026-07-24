b625706e25c0160c8785a5cf1aaacd44737087f6
ryanlau1220 <liujunhong20@gmail.com>
2026-07-24 21:26:32 +0800

    initialize Spring Boot application

---
+++ apps/backend/src/main/java/com/apu/asc/ApuAscApplication.java
@@ -0,0 +1,12 @@
+package com.apu.asc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuAscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApuAscApplication.class, args);
+    }
+}
