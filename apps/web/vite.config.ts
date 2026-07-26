3c4af349e427bcb8bd71353718a0a3b2807a53b4
ryanlau1220 <liujunhong20@gmail.com>
2026-07-26 18:32:50 +0800

    configure dev server proxy rules

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,26 @@ import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 const config = defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+      '/oauth2': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+      '/login': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+      '/logout': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 })
 
