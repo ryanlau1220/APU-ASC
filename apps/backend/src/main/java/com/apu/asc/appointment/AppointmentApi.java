78590a1f5cd0e027311e0d13b1c522fe732225ab
ryanlau1220 <liujunhong20@gmail.com>
2026-07-31 17:37:19 +0800

    add interface methods inside AppointmentApi

--- apps/backend/src/main/java/com/apu/asc/appointment/AppointmentApi.java
+++ apps/backend/src/main/java/com/apu/asc/appointment/AppointmentApi.java
@@ -5,9 +5,17 @@ import java.util.List;
 public interface AppointmentApi {
   List<AppointmentDto> findAllAppointments();
 
+  AppointmentDto getAppointmentById(String id);
+
   List<AppointmentDto> findByCustomer(String customerId);
 
   List<AppointmentDto> findByTechnician(String technicianId);
 
   AppointmentDto createAppointment(AppointmentDto appointmentDto);
+
+  AppointmentDto updateAppointment(String id, AppointmentDto appointmentDto);
+
+  AppointmentDto updateStatus(String id, String status);
+
+  void deleteAppointment(String id);
 }
