68ad97ae0cd4a49a53e596b1e1ba0e014ea65e89
ryanlau1220 <liujunhong20@gmail.com>
2026-08-18 12:45:39 +0800

    enable documenter to generate spring modulith built in component and module diagrams

--- apps/backend/src/test/java/com/apu/asc/ApuAscModulithTests.java
+++ apps/backend/src/test/java/com/apu/asc/ApuAscModulithTests.java
@@ -2,13 +2,24 @@ package com.apu.asc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 class ApuAscModulithTests {
 
+  ApplicationModules modules = ApplicationModules.of(ApuAscApplication.class);
+
   @Test
   void verifyModulithArchitecture() {
-    ApplicationModules modules = ApplicationModules.of(ApuAscApplication.class);
     modules.forEach(System.out::println);
     modules.verify();
   }
+
+  @Test
+  void generateDocumentation() {
+    new Documenter(modules)
+        .writeModulesAsPlantUml()
+        .writeIndividualModulesAsPlantUml()
+        .writeModuleCanvases();
+  }
 }
+
